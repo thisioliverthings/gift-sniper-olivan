f3fe30f1c6658967ea0ad58b45e92a759f3766c4
installer <installer@gmail.com>
2025-06-04 17:14:33 +0300

    Revert "try add tg client"
    This reverts commit 5d9188a781bc679499ade34fc35b7e8781cd1355.

--- src/client/__init__.py
+++
@@ -1,3 +0,0 @@
-from .account import get_session, send_gift
-
-__all__ = ["get_session", "send_gift"]
--- src/client/account.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client
-
-
-async def get_session():
-    pass
-
-async def send_gift(client: Client, user_id: int, gift_id: int):
-    pass

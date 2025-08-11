e775f0fb3eea3afa69c8fba888eaa6c04bf330df
thisioliverthings <Abodeking19999@gmail.com>
2025-08-11 15:47:58 +0300

    Update buttons.py

--- src/data/buttons.py
+++ src/data/buttons.py
@@ -3,22 +3,26 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, \
                             ReplyKeyboardMarkup, KeyboardButton
 
 class Markup:
+    # قائمة البداية
     start = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='👤 Профиль')],
-        [KeyboardButton(text='ℹ️ Информация')],
+        [KeyboardButton(text='👤 الملف الشخصي')],
+        [KeyboardButton(text='ℹ️ المعلومات')],
     ], resize_keyboard=True)
 
+    # قائمة الملف الشخصي
     profile = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='Пополнить баланс', callback_data='top_up')],
-        [InlineKeyboardButton(text='🚀 Приобрести VIP', callback_data='buy_vip')]
+        [InlineKeyboardButton(text='شحن الرصيد', callback_data='top_up')],
+        [InlineKeyboardButton(text='🚀 شراء VIP', callback_data='buy_vip')]
     ])
 
+    # زر إلغاء الفاتورة
     cancel_invoice = ReplyKeyboardMarkup(keyboard=[
-        [KeyboardButton(text='Отмена')]
+        [KeyboardButton(text='إلغاء')]
     ], resize_keyboard=True)
 
+    # زر شراء VIP للأبد
     buy_vip = InlineKeyboardButton(
-        text='Приобрести навсегда', callback_data='invoice_buy_vip'
+        text='شراء للأبد', callback_data='invoice_buy_vip'
     )
 
     @staticmethod
@@ -26,23 +30,23 @@ class Markup:
         is_active = lambda _mode: '✅' if mode == _mode else ''
         return InlineKeyboardMarkup(
             inline_keyboard=[
-                [InlineKeyboardButton(text=f'{is_active(0)} На весь баланс', callback_data='dev')],
-                [InlineKeyboardButton(text=f'{is_active(1)} Процент от баланса', callback_data='dev')],
-                [InlineKeyboardButton(text=f'{is_active(2)} Лимит звезд', callback_data='dev')],
+                [InlineKeyboardButton(text=f'{is_active(0)} على كامل الرصيد', callback_data='dev')],
+                [InlineKeyboardButton(text=f'{is_active(1)} نسبة من الرصيد', callback_data='dev')],
+                [InlineKeyboardButton(text=f'{is_active(2)} حد النجوم', callback_data='dev')],
                 [Markup.back('info')]
             ]
         )
-    
+
     @staticmethod
     def settings(admin_url: str) -> InlineKeyboardMarkup:
         return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text='Настройки', callback_data='settings')],
-        [InlineKeyboardButton(text='🖥 Администрация', url=admin_url)]
+        [InlineKeyboardButton(text='الإعدادات', callback_data='settings')],
+        [InlineKeyboardButton(text='🖥 الإدارة', url=admin_url)]
     ])
 
     @staticmethod
     def back(back_type: str) -> InlineKeyboardButton:
-        return InlineKeyboardButton(text='⬅️ Назад', callback_data=f'back|{back_type}')
+        return InlineKeyboardButton(text='⬅️ رجوع', callback_data=f'back|{back_type}')
 
     @staticmethod
     def configurator(*buttons: Tuple[List[InlineKeyboardButton]]) -> InlineKeyboardMarkup:
